c48a2e5a3798c5c4b7ded94291ea269f3f571575
Makochaaan <arowana326.315@gmail.com>
2022-05-25 15:40:47 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,118 @@
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 15,
+
+    gestureHandling: "greedy",
+  });
+  infoWindow = new google.maps.InfoWindow();
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.open(map);
+          map.setCenter(pos);
+
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  }
+
+  
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+function initAutocomplete() {
+
+service = new google.maps.places.AutocompleteService()
+
+inputDeparture = document.getElementById('autocompleteDeparture')
+inputArrival = document.getElementById('autocompleteArrival')
+
+options = {
+  bounds: defaultBounds,
+  fields: ["address_components", "geometry", "icon", "name"],
+  strictBounds: false,
+  types: ["establishment"],
+};
+
+autocompleteDeparture = new google.maps.places.Autocomplete(inputDeparture,options);
+autocompleteArrival = new google.maps.places.Autocomplete(inputArrival,options);
+
+}
+
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  const placeDeparture = autocompleteDeparture.getPlace();
+  const placeArrival = autocompleteArrival.getPlace();
+  let address1 = "";
+  let postcode = "";
+
+  // Get each component of the address from the place details,
+  // and then fill-in the corresponding field on the form.
+  // place.address_components are google.maps.GeocoderAddressComponent objects
+  // which are documented at http://goo.gle/3l5i5Mr
+  for (const component of place.address_components) {
+    const componentType = component.types[0];
+
+    switch (componentType) {
+      case "street_number": {
+        address1 = `${component.long_name} ${address1}`;
+        break;
+      }
+
+      case "route": {
+        address1 += component.short_name;
+        break;
+      }
+
+      case "postal_code": {
+        postcode = `${component.long_name}${postcode}`;
+        break;
+      }
+
+      case "postal_code_suffix": {
+        postcode = `${postcode}-${component.long_name}`;
+        break;
+      }
+      case "locality":
+        document.querySelector("#locality").value = component.long_name;
+        break;
+      case "administrative_area_level_1": {
+        document.querySelector("#state").value = component.short_name;
+        break;
+      }
+      case "country":
+        document.querySelector("#country").value = component.long_name;
+        break;
+    }
+  }
+
+  address1Field.value = address1;
+  postalField.value = postcode;
+  // After filling the form with address components from the Autocomplete
+  // prediction, set cursor focus on the second address line to encourage
+  // entry of subpremise information such as apartment, unit, or floor number.
+  address2Field.focus();
+}
